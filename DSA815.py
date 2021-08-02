1f1f5c4910ddb2a14f8b8860323bf0ea780e37ee
DarioAlaniz <dario_a_066@hotmail.com.ar>
2021-08-02 15:05:51 -0300

    class dsa815

---
+++ DSA815.py
@@ -0,0 +1,24 @@
+from _typeshed import Self
+import vxi11
+import unittest
+
+class DSA815(vxi11.Instrument):
+    def __init__(self, host, *args, **kwargs):
+        super(DSA815, self).__init__(host, *args, **kwargs)
+    def get_identification(self):
+        return self.ask("*IDN?")
+    def set_center_frequency(self,frequency):
+        self.write("SENSe:FREQuency:CENTer {}".format(frequency))
+        print("Center Frequency: ", self.ask("SENSe:FREQuency:CENTer?"))    
+
+
+########################################################################
+
+
+def main():
+    instrument = DSA815('172.16.0.110')
+    print(instrument.get_identification())
+    print(instrument.ask("HCOPy:PAGE:PRINts? "))
+ 
+if __name__ == "__main__":
+    main()
